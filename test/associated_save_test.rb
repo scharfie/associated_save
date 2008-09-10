39cb15b744dfb729d00f263aafbccdad2f14f558
Chris Scharf <scharfie@gmail.com>
2008-09-10 11:34:59 -0400

    Initial testing

--- test/associated_save_test.rb
+++ test/associated_save_test.rb
@@ -1,8 +1,22 @@
-require 'test/unit'
+require File.dirname(__FILE__) + '/abstract_unit'
+
+class Collection < ActiveRecord::Base
+  has_many :items
+  associated_save :items
+end
+
+class Item < ActiveRecord::Base
+  belongs_to :collection
+end
 
 class AssociatedSaveTest < Test::Unit::TestCase
-  # Replace this with your real tests.
-  def test_this_plugin
-    flunk
+  def setup
+    @library = Collection.create(:name => 'Book Library')
+  end
+  
+  def test_reflection
+    reflection = Collection.reflect_on_associated_save(:items)
+    assert_equal '_items', reflection[:from]
+    assert_equal 'save_associated_items', reflection[:callback]
   end
 end
